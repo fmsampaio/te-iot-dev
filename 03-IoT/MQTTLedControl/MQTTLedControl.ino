ce59842050f614b33ff47e59ca6bfff1b23f097b
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2022-11-09 14:18:59 -0300

    MQTTLedControl example source code

---
+++ 03-IoT/MQTTLedControl/MQTTLedControl.ino
@@ -0,0 +1,110 @@
+/*
+  Exemplo: 03-IoT/MQTTSimple
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+//TODO 
+byte mac[]    = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED}; //TODO modificar aqui!!
+
+/** MQTT TE Server **/
+String server = "m14.cloudmqtt.com";
+int port = 12891;
+String username = "vqlvmcfj";
+String password = "Vzw6NIX4voxY";
+
+String clientName = "arduinoClientXXX"; //TODO modificar aqui! Cada cliente MQTT deve ter um nome único!
+
+int ledPin = 13;
+
+// Aqui são tratadas as mensagens que são recebidas do servidor MQTT
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  String message = "";
+  
+  for (int i=0;i<length;i++) {
+    message += (char)payload[i];
+  }
+
+  String topicStr(topic);
+  Serial.println(message);
+
+  if(topicStr.equals("te/ledXX")) {
+    if(message.equals("1")) {
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(message.equals("0")) {
+      digitalWrite(ledPin, LOW);
+    }
+    else {
+      Serial.println("Command not recognized...");
+    }
+  }
+
+}
+
+// Criação e inicialização dos objetos para a criação de um cliente MQTT
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+int cont = 0;
+
+// Função para criação da conexão com o MQTT Broker
+void connectToMQTTBroker() {
+
+  // Tentativa (de forma repetida) de conexão
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect(clientName.c_str(), username.c_str(), password.c_str())) { 
+      Serial.println("connected");
+    } 
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("trying again in 5 seconds");
+      // Aguarda 5 segundos antes de tentar novamente a conexão
+      delay(5000);
+    }
+  }
+  
+}
+
+//Aqui acontecem as inscrições em tópicos
+void subscribeToTopics() {
+  client.subscribe("te/ledXX"); //TODO: modificar sufixo do nome do tópico
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  delay(1500);
+
+  Serial.println("Interface ethernet iniciada...");
+  Serial.println(Ethernet.localIP());
+
+  client.setServer(server.c_str(), port);
+  client.setCallback(callback);
+
+  connectToMQTTBroker();
+  subscribeToTopics();
+
+  pinMode(ledPin, OUTPUT);
+  
+}
+
+void loop()
+{
+  if(!client.connected()) {
+    connectToMQTTBroker();
+    subscribeToTopics();
+  }
+
+  client.loop();
+}
