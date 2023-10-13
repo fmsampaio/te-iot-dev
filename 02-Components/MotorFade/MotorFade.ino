b6f3897cac812b834aad59324b47ff801b35e4e0
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2023-10-13 10:41:12 -0300

    MotorFade example to control DC Motor

--- 02-Components/MotorFade/MotorFade.ino
+++ 02-Components/MotorFade/MotorFade.ino
@@ -4,31 +4,39 @@ int IN1 = 2 ;
 int IN2 = 4 ;
 int velocidadeA = 3;
 
-//variavel auxiliar
-int velocidade = 0;
 //Inicializa Pinos
 void setup(){
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
   pinMode(velocidadeA,OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop(){
   /*Exemplo de velocidades no motor A*/
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
+  Serial.println("Sentido anti-horário!");
 
- 
-  //velocidade de 0 a 255
-  while (velocidade < 255){
+  int velocidade;
+  for(velocidade = 0; velocidade < 255; velocidade += 10) {
+    Serial.print("Velocidade: ");
+    Serial.println(velocidade);
     analogWrite(velocidadeA,velocidade);
-    velocidade = velocidade + 10;
-    delay(50);
+    delay(500);
   }
-  //velocidade de 255 a 0
-  while (velocidade > 0){
+
+  /*Exemplo de velocidades no motor A*/
+  digitalWrite(IN1,LOW);
+  digitalWrite(IN2,HIGH);
+  Serial.println("Sentido HORÁRIO!");
+
+  for(velocidade = 255; velocidade > 0; velocidade -= 10) {
+    Serial.print("Velocidade: ");
+    Serial.println(velocidade);
     analogWrite(velocidadeA,velocidade);
-    velocidade = velocidade - 10;
-    delay(50);
-}
+    delay(500);
+  }
+  
 }
