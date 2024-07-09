07c6e36e01c5c8795485ef3ecc45483a1eb3544c
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2024-07-09 15:34:50 -0300

    MQTTSensor example to publish LDR (A0) data

---
+++ 03-IoT/MQTTSensor/MQTTSensor.ino
@@ -0,0 +1,86 @@
+/*
+  Exemplo: 03-IoT/MQTTSimple
+*/
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+
+// TODO: cada bancada deve ter um MAC address diferente
+byte mac[]    = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED}; //TODO modificar aqui!!
+
+/** Plano A => MQTT Eclipse Projects**/
+String server = "mqtt.eclipseprojects.io";
+int port = 1883;
+
+/** Plano B => Public MQTT HQ **/
+//String server = "public.mqtthq.com";
+//int port = 1883;
+
+String ldrTopic = "te/ldrXX";
+String clientName = "arduinoClientXX"; //TODO modificar aqui! Cada cliente MQTT deve ter um nome único!
+
+int ldrPin = A0;
+
+// Criação e inicialização dos objetos para a criação de um cliente MQTT
+EthernetClient ethClient;
+PubSubClient client(ethClient);
+
+// Função para criação da conexão com o MQTT Broker
+void connectToMQTTBroker() {
+
+  // Tentativa (de forma repetida) de conexão
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect(clientName.c_str())) { 
+      Serial.println("connected");
+    } 
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("trying again in 5 seconds");
+      // Aguarda 5 segundos antes de tentar novamente a conexão
+      delay(5000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  // Inicializando o Ethernet Shield
+  Ethernet.begin(mac);
+  delay(1500);
+
+  Serial.println("Interface ethernet iniciada...");
+
+  // Setando informações do Broker MQTT e da função de callback
+  client.setServer(server.c_str(), port);
+
+  connectToMQTTBroker();  
+}
+
+void publishLuminosity() {
+  int luminosity = analogRead(ldrPin);
+  String lumStr = String(luminosity);
+
+  client.publish(ldrTopic.c_str(), lumStr.c_str());
+  Serial.print("Message published [");
+  Serial.print(ldrTopic);
+  Serial.print("] --> ");
+  Serial.println(luminosity);
+}
+
+void loop()
+{
+  if(!client.connected()) {
+    connectToMQTTBroker();
+    
+  }
+  publishLuminosity();
+  delay(1000);
+
+  client.loop();
+}
